34dbf525000f1526d7762963d00bc629f94c84ba
greenteak <greenteak1992@gmail.com>
2018-01-16 19:30:52 +0800

    homework3 0.1

--- src/CompareNumber/CompareNumber.java
+++ src/CompareNumber/CompareNumber.java
@@ -2,7 +2,16 @@ package CompareNumber;
 
 public class CompareNumber {
    public String CompareAnswerAndInput(String InputNumber,String AnswerNumber){
-
-       return InputNumber;
+       String output=new String("");
+       int NumA=0;
+       int NumB=0;
+       for(int i=0;i<InputNumber.length();i++){
+           if(InputNumber.charAt(i)==AnswerNumber.charAt(i)){
+               NumA++;
+           }
+           else NumB++;
+       }
+       output+=(String.valueOf(NumA)+"A"+String.valueOf(NumB)+"B");
+       return output;
    }
 }
